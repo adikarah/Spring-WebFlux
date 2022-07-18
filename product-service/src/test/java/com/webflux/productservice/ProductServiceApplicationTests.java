16df5f37812795b1d442d88a5fba7aef57ff46ca
Tripathi, Praveen Kumar <pravtripathi@deloitte.com>
2022-07-18 23:41:36 +0530

    added product micor-service

---
+++ product-service/src/test/java/com/webflux/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webflux.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
